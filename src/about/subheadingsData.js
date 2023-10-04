37c05a84d515508ae78e8b88502d85193d9f275a
FullStackJinn <otsjlellc@gmail.com>
2023-10-04 10:14:54 -0700

    adding in the about menu items and data. as well as functionality of the onclick for the menu items

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,90 @@
+const subheadingsData = {
+    1: [
+        {
+            title: "About Me",
+            content: (
+                <>
+                <p>
+                    Lorem ipsum dolor sit amet
+                </p>
+                </>
+            ),
+        },
+    ],
+2: [
+    {
+        title: "2016 Masters Degree",
+        content: (
+            <>
+            <p>
+                Lorem ipsum dolor sit amet
+            </p>
+            </>
+        ),
+    },
+    {
+        title: "2016 Masters Degree",
+        content: (
+            <>
+            <p>
+                Lorem ipsum dolor sit amet
+            </p>
+            </>
+        ),
+    },
+    {
+        title: "2016 Masters Degree",
+        content: (
+            <>
+            <p>
+                Lorem ipsum dolor sit amet
+            </p>
+            </>
+        ),
+    },
+    {
+        title: "2016 Masters Degree",
+        content: (
+            <>
+            <p>
+                Lorem ipsum dolor sit amet
+            </p>
+            </>
+        ),
+    },
+],
+3: [
+    {
+        title: "2022 Front End Developer",
+        content: (
+            <>
+            <p>
+                Lorem ipsum dolor sit amet
+            </p>
+            </>
+        ),
+    },
+    {
+        title: "2020 UX Designer",
+        content: (
+            <>
+            <p>
+                Lorem ipsum dolor sit amet
+            </p>
+            </>
+        ),
+    },
+    {
+        title: "2017 IT consultant",
+        content: (
+            <>
+            <p>
+                Lorem ipsum dolor sit amet
+            </p>
+            </>
+        ),
+    },
+],
+};
+
+export default subheadingsData;
--- src/skills/SkillsMenu.js
+++ src/skills/SkillsMenu.js
@@ -1,4 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import '../styles/skillsMenu.css';
+import classNames from 'classnames';
+import skills from './skillsData.js';
+import frontendIcon from '../assets/eagle-emblem.png';
+import backendIcon from '../assets/hawk-emblem.png';
 
 export default class SkillsMenu extends Component {
   render() {
