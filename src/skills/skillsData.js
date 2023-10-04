d34438640c596cb122cec7908de5d7337ef68cdc
FullStackJinn <otsjlellc@gmail.com>
2023-10-04 12:40:12 -0700

    skills data, menu and the css

---
+++ src/skills/skillsData.js
@@ -0,0 +1,41 @@
+const skills = {
+    1: [ {
+        title: "HTML",
+        level: 4,
+    },
+    {
+        title: "CSS",
+        level: 4,
+    },
+    {
+        title: "Javascript",
+        level: 4,
+    },
+    {
+        title: "React",
+        level: 4,
+    },
+    ],
+    2: [ {
+        title: "Node.js",
+        level: 4,
+    },
+    {
+        title: "SQL",
+        level: 4,
+    },
+    {
+        title: "MongoDB",
+        level: 4,
+    },
+    {
+        title: "Git",
+        level: 4,
+    },
+    {
+        title: "Express.js",
+        level: 4,
+    },
+    ],
+};
+export default skills;
