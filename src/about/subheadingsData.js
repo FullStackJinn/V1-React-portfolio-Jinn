a51588e62d6cb0313e79ee74d4ef57b903db2962
FullStackJinn <otsjlellc@gmail.com>
2023-10-06 12:40:58 -0700

    add in personal info and style

--- src/about/subheadingsData.js
+++ src/about/subheadingsData.js
@@ -16,18 +16,12 @@ const subheadingsData = {
         title: "Full-Stack Web Development Bootcamp",
         content: (
             <>
+            <h4>2022-2023</h4>
             <p>
-                Lorem ipsum dolor sit amet
-            </p>
-            </>
-        ),
-    },
-    {
-        title: "Node.js Developer Course",
-        content: (
-            <>
-            <p>
-                Lorem ipsum dolor sit amet
+                Taught by Dr.Angela Yu. This bootcamp covers front and 
+                back end deveopment using a massive amount of tools and technologies,
+                including: HTML5, CSS3, Javascript ES6, NODE.js, REACT,js, EXPRESS,js.
+                
             </p>
             </>
         ),
@@ -36,8 +30,26 @@ const subheadingsData = {
         title: "React - The Complete Guide",
         content: (
             <>
+            <h4>2023-Present</h4>
             <p>
-                Lorem ipsum dolor sit amet
+               Taught by Maximillian Schwarzmuller of Academind. This course covers an introduction
+               to React.js, core basics, Components, Props, Dynamic Data Binding, React Hooks,
+               Redux, React Router, Intro to Next.js, and Combining React with TypeScript
+            </p>
+            </>
+        ),
+    },
+    {
+        title: "Node.js Developer Course",
+        content: (
+            <>
+            <h4>2023-Present</h4>
+            <p>
+                Taught by Andrei Neagoie of ZTM Academy. This course covers websockets, GraphQL,
+                Node Production and AWS, Deployment and CI/CD, Authentication,
+                Working with REST APIs, Databases, Improving Node Performance,
+                Testing APIs, Express.js, Web Servers, Package Management, Module System,
+                and Foundations of Node.
             </p>
             </>
         ),
@@ -45,12 +57,18 @@ const subheadingsData = {
 ],
 3: [
     {
-        title: "Junior Frontend Developer",
+        title: "Junior Website Developer",
         content: (
             <>
-            <p>
-                Lorem ipsum dolor sit amet
-            </p>
+            <h4>July 2022 - Present</h4>
+            <ul>
+                <li>Implemented responsive design techniques for e-commerce site, ensuring optimal user experience across various devices and screen sizes.
+</li>
+                <li>Conducted code reviews and testing to identify and fix bugs, and ensure compatibility with different browsers and devices. Effectively removing 90% of website errors since launch
+</li>
+                <li>Integrate product recommendation quiz  with a 90.72% completion rate during the first 30 days
+</li>
+            </ul>
             </>
         ),
     },
