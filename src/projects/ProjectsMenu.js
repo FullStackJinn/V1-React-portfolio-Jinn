d6357497f5391f86dee664428c9093e80744687f
FullStackJinn <otsjlellc@gmail.com>
2023-10-07 17:40:05 -0700

    Update ProjectsMenu.js

--- src/projects/ProjectsMenu.js
+++ src/projects/ProjectsMenu.js
@@ -37,7 +37,7 @@ export default class ProjectsMenu extends Component {
 
   render() {
     const { activeProject } = this.state;
-    const projectItems = ["PROJECT ONE", "PROJECT TWO", "PROJECT THREE"];
+    const projectItems = ["Web Portfolio", "PROJECT TWO", "PROJECT THREE"];
 
     return (
       <div className="project-menu">
